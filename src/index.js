c6c7972e6a0e7b3682914fa294618076e2b4d791
Prabhudas-M <60952658+Prabhudas-M@users.noreply.github.com>
2020-11-25 08:58:38 -0500

    dsd

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+import animals from "./data";
+//const col = {color:"#945290"};
 
+console.log(animals);
 // ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
+// <h1 style = {col}>welcome</h1>,
+  // <table>
+  //   <tr>
+  //     <th>Brand</th>
+  //     <th>Top Speed</th>
+  //   </tr>
+  //   <tr>
+  //     <td>{tesla.model}</td>
+  //     <td>{teslaTopSpeed}</td>
+  //     <td>{teslaTopColour}</td>
+  //   </tr>
+  //   <tr>
+  //     <td>{honda.model}</td>
+  //     <td>{hondaTopSpeed}</td>
+  //     <td>{hondaTopColour}</td>
+  //   </tr>
+  // </table>,
 //   document.getElementById("root")
 // );
