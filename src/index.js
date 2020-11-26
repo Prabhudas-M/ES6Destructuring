dc7ec8f878726c35d506834d0af72c5ef3ff71c0
Prabhudas-M <60952658+Prabhudas-M@users.noreply.github.com>
2020-11-25 21:58:14 -0500

    s

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 import animals from "./data";
 import Cars from "./practice";
 
+console.log(Cars);
 //console.log(animals)
 
 // const[cat,dog] = animals;
@@ -14,22 +15,24 @@ import Cars from "./practice";
 // CHALLENGE: uncomment the code below and see the car stats rendered
 
 ReactDOM.render(
-  <h1 style={col}>welcome</h1>,
-  <table>
-    <tr>
-      <th>Brand</th>
-      <th>Top Speed</th>
-    </tr>
-    <tr>
-      {/* <td>{tesla.model}</td>
+  <div>
+    <h1>welcome</h1>,
+    <table>
+      <tr>
+        <th>Brand</th>
+        <th>Top Speed</th>
+      </tr>
+      {/* <tr>
+      <td>{tesla.model}</td>
       <td>{teslaTopSpeed}</td>
-      <td>{teslaTopColour}</td> */}
+      <td>{teslaTopColour}</td>
     </tr>
     <tr>
-      {/* <td>{honda.model}</td>
+      <td>{honda.model}</td>
       <td>{hondaTopSpeed}</td>
-      <td>{hondaTopColour}</td> */}
-    </tr>
-  </table>,
+      <td>{hondaTopColour}</td>
+    </tr> */}
+    </table>{" "}
+  </div>,
   document.getElementById("root")
 );
